77156ac76b8680a7bd0d79cd6e2fe043a635a827
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-08-19 11:38:07 -0600

    servicio, paginas

---
+++ 03-pokedex-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '03-pokedex-app',
+  webDir: 'www'
+};
+
+export default config;
